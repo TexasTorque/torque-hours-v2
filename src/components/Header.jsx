ecb0b458f86c3faf8aa4dd4cab0b042aa4a34d31
humandavey <davey.adams.three@gmail.com>
2023-10-02 17:22:00 -0500

    Header back to home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
+import { useNavigate } from "react-router-dom";
 import torque_logo from "../assets/torque_logo.png";
 
 export default function Header() {
+
+  const navigate = useNavigate();
+
   return (
-    <div className="header">
+    <div className="header" onClick={() => navigate("/")}>
       <div className="header-border-box">
         <img src={torque_logo} style={{ height: "3.5em", width: "3.5em" }}></img>
         <div>
